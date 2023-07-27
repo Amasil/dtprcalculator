32bfd0f092f511d2e342a43d84d94a7c8c1a9f10
Amasil Rahim Zihad <amasilrahimzihad@gmail.com>
2023-07-26 20:30:27 -0600

    Improvments

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 document.addEventListener("DOMContentLoaded", function () {
+  const clientName = document.getElementById("claimant_name");
+  const clientClinic = document.getElementById("clinic_name");
+  const invNum = document.getElementById("invoice_number");
+
   const qtyAB1 = document.getElementById("qty_ab1");
   const priceAB1 = document.getElementById("price_ab1");
   const qtyAB2 = document.getElementById("qty_ab2");
@@ -8,6 +12,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
   const qty83PT = document.getElementById("qty_83PT");
   const price83PT = document.getElementById("price_83PT");
+  const dateField = document.getElementById("date_83PT");
   const qty41PT = document.getElementById("qty_41PT");
   const price41PT = document.getElementById("price_41PT");
 
@@ -16,20 +21,46 @@ document.addEventListener("DOMContentLoaded", function () {
   const qty41CT = document.getElementById("qty_41CT");
   const price41CT = document.getElementById("price_41CT");
 
-  const totalValue = document.getElementById("total_value");
-
-  // Function to calculate the total price
+  const totalValueElement = document.getElementById("total_value");
+  const totalQuantityElement = document.getElementById("total_quantity");
+  const selectedItemsContainer = document.getElementById("selected_items");
+  // Function to calculate the total price and quantity
   function calculateTotal() {
-    const total =
-      parseFloat(priceAB1.value) +
-      parseFloat(priceAB2.value) +
-      parseFloat(priceAB4.value) +
-      parseFloat(price83PT.value) +
-      parseFloat(price41PT.value) +
-      parseFloat(price83CT.value) +
-      parseFloat(price41CT.value);
+    const anyItemSelected =
+      parseFloat(qtyAB1.value) > 0 ||
+      parseFloat(qtyAB2.value) > 0 ||
+      parseFloat(qtyAB4.value) > 0 ||
+      parseFloat(qty83PT.value) > 0 ||
+      parseFloat(qty41PT.value) > 0 ||
+      parseFloat(qty83CT.value) > 0 ||
+      parseFloat(qty41CT.value) > 0;
+    const itemQuantities = [
+      parseFloat(qty83PT.value),
+      parseFloat(qty41PT.value),
+      parseFloat(qty83CT.value),
+      parseFloat(qty41CT.value),
+    ];
+    const itemPrices = [
+      parseFloat(priceAB1.value),
+      parseFloat(priceAB2.value),
+      parseFloat(priceAB4.value),
+      parseFloat(price83PT.value),
+      parseFloat(price41PT.value),
+      parseFloat(price83CT.value),
+      parseFloat(price41CT.value),
+    ];
+    const totalQty = itemQuantities.reduce((acc, qty) => acc + qty, 0);
 
-    totalValue.innerText = total.toFixed(2);
+    const totalPrice = itemPrices.reduce((acc, price) => acc + price, 0);
+    // const totalQty = itemQuantities.reduce((acc, qty) => acc + qty, 0);
+    // Hide "Total Quantity" if totalQty is 0
+    if (anyItemSelected > 0) {
+      const totalQuantityDisplay = document.createElement("p");
+      totalQuantityDisplay.innerText = ` DTPR ${totalQty}/21 | Supplies:$ | Used by`;
+      selectedItemsContainer.appendChild(totalQuantityDisplay);
+    }
+    totalValueElement.innerText = totalPrice.toFixed(2);
+    // totalQuantity.innerText = totalQty.toFixed(2);
   }
 
   // Function to update the displayed item price and quantity
@@ -52,42 +83,67 @@ document.addEventListener("DOMContentLoaded", function () {
 
   // Function to display selected items
   function displaySelectedItems() {
-    const selectedItemsContainer = document.getElementById("selected_items");
     selectedItemsContainer.innerHTML = "";
 
-    // Function to add an item to the display box
-    function addItemToDisplay(itemName, qtyValue, priceValue) {
+    // Calculate total value
+    const totalValue = parseFloat(totalValueElement.innerText);
+    const totalValueDisplay = document.createElement("p");
+
+    const clientName = document.getElementById("claimant_name").value;
+    const invNum = document.getElementById("invoice_number").value;
+    const clientClinic = document.getElementById("clinic_name").value;
+    totalValueDisplay.innerText = `Claimant: ${clientName} \n Paid Inv. ${invNum} to ${clientClinic} for $${totalValue.toFixed(
+      2
+    )}`;
+    selectedItemsContainer.appendChild(totalValueDisplay);
+
+    /* AB Items Display */
+    function addABItemToDisplay(itemName, qtyValue, priceValue) {
       const itemDisplay = document.createElement("p");
-      itemDisplay.innerText = `${qtyValue} ${itemName} @ $${priceValue.toFixed(
-        2
-      )}`;
+      itemDisplay.innerText = `${qtyValue} ${itemName}`;
+      selectedItemsContainer.appendChild(itemDisplay);
+    }
+
+    /* Fixed Value Items Display */
+    function addItemToDisplay(itemName, qtyValue, priceValue, dateValue) {
+      const itemDisplay = document.createElement("p");
+      itemDisplay.innerText = `${qtyValue} ${itemName} - Date: ${dateValue}`;
       selectedItemsContainer.appendChild(itemDisplay);
     }
 
     const qtyAB1Value = parseFloat(qtyAB1.value);
     const priceAB1Value = parseFloat(priceAB1.value);
     if (qtyAB1Value > 0) {
-      addItemToDisplay("AB1 ", qtyAB1Value, priceAB1Value);
+      addABItemToDisplay("AB1 @ $117", qtyAB1Value, priceAB1Value);
     }
 
     const qtyAB2Value = parseFloat(qtyAB2.value);
     const priceAB2Value = parseFloat(priceAB2.value);
     if (qtyAB2Value > 0) {
-      addItemToDisplay("AB2 ", qtyAB2Value, priceAB2Value);
+      addABItemToDisplay("AB2 @ $50", qtyAB2Value, priceAB2Value);
     }
 
     const qtyAB4Value = parseFloat(qtyAB4.value);
     const priceAB4Value = parseFloat(priceAB4.value);
     if (qtyAB4Value > 0) {
-      addItemToDisplay("AB4 ", qtyAB4Value, priceAB4Value);
+      addABItemToDisplay("AB4 @ $40", qtyAB4Value, priceAB4Value);
     }
 
     // Get values of selected items
     const qty83PTValue = parseFloat(qty83PT.value);
     const price83PTValue = parseFloat(price83PT.value);
+    const date83PTValue = document.getElementById("date_83PT").value;
     if (qty83PTValue > 0) {
-      addItemToDisplay("PT @ $83 Each", qty83PTValue, price83PTValue);
+      addItemToDisplay(
+        "PT @ $83 Each",
+        qty83PTValue,
+        price83PTValue,
+        date83PTValue
+      );
     }
+    dateField.addEventListener("input", function () {
+      displaySelectedItems(); // Update "Paid Inv." section when date is changed
+    });
 
     const qty41PTValue = parseFloat(qty41PT.value);
     const price41PTValue = parseFloat(price41PT.value);
@@ -107,13 +163,20 @@ document.addEventListener("DOMContentLoaded", function () {
     if (qty41CTValue > 0) {
       addItemToDisplay("CT @ $41 Each", qty41CTValue, price41CTValue);
     }
-    // Add other selected items here...
 
     // Call calculateTotal to update the total price in the display box
     calculateTotal();
   }
-
-  // Event listeners to update prices when quantity inputs change
+  clientName.addEventListener("input", function () {
+    displaySelectedItems();
+  });
+  clientClinic.addEventListener("input", function () {
+    displaySelectedItems();
+  });
+  invNum.addEventListener("input", function () {
+    displaySelectedItems();
+  });
+  // Event listeners to update prices and quantities when quantity inputs change
   qtyAB1.addEventListener("input", function () {
     priceAB1.value = (qtyAB1.value * 117).toFixed(2);
     calculateTotal();
@@ -143,6 +206,7 @@ document.addEventListener("DOMContentLoaded", function () {
     calculateTotal();
     displaySelectedItems(); // Update display box
   });
+
   qty83CT.addEventListener("input", function () {
     price83CT.value = (qty83CT.value * 83).toFixed(2);
     calculateTotal();
@@ -154,12 +218,7 @@ document.addEventListener("DOMContentLoaded", function () {
     calculateTotal();
     displaySelectedItems(); // Update display box
   });
-  // Displayed item elements for 83PT and 41PT
-  const itemAB1 = document.getElementById("item_ab1");
-  const itemAB2 = document.getElementById("item_ab2");
-  const itemAB4 = document.getElementById("item_ab4");
-  const item83PT = document.getElementById("item_83PT");
-  const item41PT = document.getElementById("item_41PT");
-  const item83CT = document.getElementById("item_83CT");
-  const item41CT = document.getElementById("item_41CT");
+
+  // Initial calculation on page load
+  calculateTotal();
 });
